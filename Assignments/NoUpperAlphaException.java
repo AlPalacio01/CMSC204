b671de91527afa8892526b45f2f078505005da7f
Alejandro Palacio <121836852+AlPalacio01@users.noreply.github.com>
2025-02-10 17:27:49 -0500

    Delete Assignments/NoUpperAlphaException.java

--- Assignments/NoUpperAlphaException.java
+++
@@ -1,7 +0,0 @@
-package application;
-
-public class NoUpperAlphaException extends Exception {
-	public NoUpperAlphaException() {
-		super("The password must contain at least one uppercase alphabetic character");
-	}
-}
