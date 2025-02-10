3680ed81d4a8adb85846c186ffc1c0a60ec3b462
Alejandro Palacio <121836852+AlPalacio01@users.noreply.github.com>
2025-02-10 17:24:26 -0500

    Add files via upload

---
+++ Assignments/NoLowerAlphaException.java
@@ -0,0 +1,9 @@
+package application;
+
+public class NoLowerAlphaException extends Exception {
+	public NoLowerAlphaException() {
+		super("The password must contain at least one lowercase alphabetic character");
+	}
+
+
+}
