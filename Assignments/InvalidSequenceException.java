8c15df9c72db5eeb19284ffc2fa517c22fc33992
Alejandro Palacio <121836852+AlPalacio01@users.noreply.github.com>
2025-02-10 17:25:30 -0500

    Delete Assignments/InvalidSequenceException.java

--- Assignments/InvalidSequenceException.java
+++
@@ -1,9 +0,0 @@
-package application;
-
-public class InvalidSequenceException extends Exception {
-	public InvalidSequenceException() {
-		super("The password cannot contain more than two of the same character in sequence");
-	}
-
-
-}
