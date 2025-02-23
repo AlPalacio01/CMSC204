a6e5c9d781af42be600fd71c0bc99f103b75f58f
Alejandro Palacio <121836852+AlPalacio01@users.noreply.github.com>
2025-02-22 20:05:23 -0500

    Add files via upload

---
+++ Labs/Lab2/ArraySum.java
@@ -0,0 +1,14 @@
+package zLab2;
+
+public class ArraySum {
+
+	public int sumOfArray(Integer[] myArray, int index) {
+		
+		if (index < 0) {
+	        return 0;
+	    }
+		
+		return myArray[index] + sumOfArray(myArray, index - 1);
+	}
+
+}
